176b7f4c55ac5e1167622aca109ba5413e85de8d
pseeweb <wilbert.ridder@gmail.com>
2014-06-08 18:55:39 +0200

    - Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-backgrid-sizeable-columns
-=========================
+# Backgrid.js - (Re)sizeable columns PoC
+Warning! This is by far not production ready code, just a mere proof of concept. It lacks documentation, automatic testing, IE browser support (somehow, did work before) and edge-case tests.
+
+## Demo website
+Online demo of PoC can be found [here](http://techwuppet.com/backgrid_poc_demo/)
+
+## (Re)sizeable columns
+- Uses html5 valid colgroup method to (re)size columns
+- Supports nonresizeable, width, min and max width
+- Trigger event on column resize
+- Supports 'renderable' and add/removal of columns
+- Supports Grouped columns in header
+
+### Example
+
+```javascript
+
+// Add sizeable columns
+var sizeAbleCol = new Backgrid.Extension.sizeAbleColumns({
+  collection: pageableTerritories,
+  columns: columns
+});
+$backgridContainer.find('thead').before(sizeAbleCol.render().el);
+
+// Add resize handlers
+var sizeHandler = new Backgrid.Extension.sizeAbleColumnsHandlers({
+  sizeAbleColumns: sizeAbleCol,
+  grid: pageableGrid,
+  saveModelWidth: true
+});
+$backgridContainer.find('thead').before(sizeHandler.render().el);
+
+// Listen to resize events
+columns.on('resize', function(columnModel, newWidth, oldWidth) {
+  console.log('Resize event on column; name, model, new and old width: ', columnModel.get("name"), columnModel, newWidth, oldWidth);
+});
+```
+
+## License
+Copyright (c) 2014 Wilbert van de Ridder
+Licensed under the [MIT license](LICENSE-MIT "MIT License").
---
+++ backgrid-sizeable-columns.js
@@ -0,0 +1,275 @@
+/*
+ backgrid-sizeable-columns
+ https://github.com/WRidder/backgrid-sizeable-columns
+
+ Copyright (c) 2014 Wilbert van de Ridder
+ Licensed under the MIT @license.
+ */
+(function (root, factory) {
+
+  // CommonJS
+  if (typeof exports == "object") {
+    module.exports = factory(require("underscore"), require("backgrid"));
+  }
+  // Browser
+  else factory(root._, root.Backgrid, root.moment);
+
+}(this, function (_, Backgrid) {
+
+
+  // Adds width support to columns
+  Backgrid.Extension.SizeAbleColumns = Backbone.View.extend({
+    /** @property */
+    tagName: "colgroup",
+
+    /**
+     Initializer.
+
+     @param {Object} options
+     @param {Backbone.Collection.<Backgrid.Column>|Array.<Backgrid.Column>|Array.<Object>} options.columns Column metadata.
+     */
+    initialize: function (options) {
+      this.columns =  options.columns;
+      this.collection = options.collection;
+
+      // Attach event listeners once on render
+      this.listenTo(this.collection, "backgrid:header:rendered", this.render);
+    },
+
+    /**
+     Renders a column group.
+     */
+    render: function () {
+      var view = this;
+      view.$el.empty();
+
+      this.columns.each(function(col) {
+        if (typeof col.get("renderable") == "undefined" || col.get("renderable")) {
+          var $colEl = $("<col>").appendTo(view.$el).attr("data-column-id", col.cid);
+          var colWidth = col.get("width");
+          var colMinWidth = col.get("minWidth");
+          var colMaxWidth = col.get("maxWidth");
+
+          if (colWidth) {
+            if (colMinWidth && colWidth < colMinWidth) {
+              colWidth = colMinWidth;
+            }
+            if (colMaxWidth && colWidth > colMaxWidth) {
+              colWidth = colMaxWidth;
+            }
+            $colEl.width(colWidth);
+          }
+        }
+      });
+
+      // Trigger event
+      view.collection.trigger("backgrid:colgroup:updated");
+
+      return this;
+    }
+  });
+
+  // Makes column resizable; requires Backgrid.Extension.sizeAbleColumns
+  Backgrid.Extension.SizeAbleColumnsHandlers = Backbone.View.extend({
+    /**
+     Initializer.
+
+     @param {Object} options
+     @param {Backbone.Collection.<Backgrid.Column>|Array.<Backgrid.Column>|Array.<Object>} options.columns Column metadata.
+     */
+    initialize: function (options) {
+      this.grid = options.grid;
+      this.columns =  this.grid.columns;
+      this.header = this.grid.header;
+      this.sizeAbleColumns = options.sizeAbleColumns;
+      this.saveModelWidth = options.saveModelWidth;
+      this.setHeaderElements();
+      this.attachEvents();
+    },
+
+    /**
+     Renders a column group.
+     */
+    render: function () {
+      var view = this;
+      view.$el.empty();
+
+      // For now, loop tds in first row
+      _.each(view.headerElements, function(columnEl, index) {
+        // Get matching col element
+        var $column = $(columnEl);
+        var $col = view.sizeAbleColumns.$el.find("col").eq(index);
+        var columnModel = view.columns.get($col.data("column-id"));
+
+        if (columnModel.get("resizeAble") &&
+          (typeof columnModel.get("renderable") == "undefined" || columnModel.get("renderable"))) {
+
+          // Create helper elements
+          var $resizeHandler = $("<div></div>")
+            .addClass("resizeHandler")
+            .attr("data-column-index", index)
+            .appendTo(view.$el);
+          var $resizeHandlerHelper = $("<div></div>")
+            .hide()
+            .addClass("grid-draggable-cursor")
+            .appendTo($resizeHandler);
+
+          // Make draggable
+          $resizeHandler.on("mousedown", function(e) {
+            var startX = Math.round($resizeHandler.offset().left);
+            var $doc = $(document);
+            var handlerNonDragSize = $resizeHandler.outerWidth();
+
+            // Set class
+            $resizeHandler.addClass("grid-draggable");
+            $resizeHandlerHelper.show();
+
+            // Follow the mouse
+            var mouseMoveHandler = function(evt) {
+              // Check for constraints
+              var minWidth = columnModel.get("minWidth");
+              if (!minWidth || minWidth < 20) {
+                minWidth = 20;
+              }
+              var maxWidth = columnModel.get("maxWidth");
+              var newLeftPos = evt.pageX;
+              var currentWidth = $col.width();
+              var newWidth = currentWidth + (newLeftPos - startX) - handlerNonDragSize / 2;
+
+              if (minWidth && newWidth <= minWidth) {
+                newLeftPos = startX - (currentWidth - minWidth) + handlerNonDragSize / 2;
+              }
+              if (maxWidth && newWidth >= maxWidth) {
+                newLeftPos = startX + maxWidth - currentWidth + handlerNonDragSize / 2;
+              }
+
+              // Apply mouse change to handler
+              $resizeHandler.offset({
+                left: newLeftPos
+              });
+            };
+            $doc.on("mousemove", mouseMoveHandler);
+
+            // Add handler to listen for mouseup
+            var mouseUpHandler = function() {
+              // Cleanup
+              $resizeHandler.removeClass("grid-draggable");
+              $resizeHandlerHelper.hide();
+              $doc.off("mouseup", mouseUpHandler);
+              $doc.off("mousemove", mouseMoveHandler);
+
+              // Adjust column size
+              var stopX = Math.round($resizeHandler.offset().left);
+              var offset = (startX - stopX);
+              var oldWidth = $column.outerWidth();
+              var newWidth = oldWidth - offset;
+
+              // Set column width
+              $col.width(newWidth);
+              newWidth = $column.outerWidth();
+              $col.width(newWidth);
+              view.updateHandlerPosition();
+
+              if (newWidth != oldWidth) {
+                if (view.saveModelWidth) {
+                  // Save updated width
+                  columnModel.set("width", newWidth, { silent: true});
+                }
+
+                // Trigger event
+                view.columns.trigger("resize", columnModel, newWidth, oldWidth);
+              }
+            };
+            $doc.on("mouseup", mouseUpHandler);
+
+            e.preventDefault();
+          });
+        }
+      });
+
+      // Position drag handlers
+      view.updateHandlerPosition();
+
+      return this;
+    },
+    attachEvents: function() {
+      this.listenTo(this.columns, "change:resizeAble", this.render);
+      this.listenTo(this.columns, "change:width", this.updateHandlerPosition);
+      this.listenTo(this.columns, "add remove", function() {
+        this.setHeaderElements();
+        this.render();
+      }.bind(this));
+      this.listenTo(this.grid.collection, "backgrid:refresh", this.render);
+      this.listenTo(this.grid.collection, "backgrid:colgroup:updated", function() {
+        this.setHeaderElements();
+        this.render();
+      }.bind(this));
+    },
+    updateHandlerPosition: function() {
+      var view = this;
+/*      _.each(view.headerElements, function(columnEl, index) {
+       var $column = $(columnEl);
+       var $col = view.sizeAbleColumns.$el.find("col").eq(index);
+       var columnModel = view.columns.get($col.data("column-id"));
+
+       if (columnModel.get("resizeAble")) {
+       // Get handler for current column and update position
+       view.$el.children().filter("[data-column-index='" + index + "']")
+       .css("left", $column.position().left + $column.outerWidth() + "px");
+       }
+       });*/
+      var left = 0;
+      view.sizeAbleColumns.$el.find("col").each(function(index, col) {
+        var $col = $(col);
+        var columnModel = view.columns.get($col.data("column-id"));
+        var $column = $(view.headerElements[index]);
+
+        if (columnModel.get("resizeAble")) {
+          // Get handler for current column and update position
+          view.$el.children().filter("[data-column-index='" + index + "']")
+            .css("left", $column.position().left + $column.outerWidth() + "px");
+        }
+        left += $(col).width();
+      });
+    },
+    setHeaderElements: function() {
+      var view = this;
+      var $headerEl = view.grid.header.$el;
+      var $rows = $headerEl.children("tr");
+      view.headerElements = [];
+
+      // Loop rows to find header cells; currently this method does not support header columns with a colspan > 1.
+      if ($rows.length < 2) {
+        view.headerElements = $rows.children();
+      }
+      else {
+        // Get all rows in the header
+        var rowAmount = $rows.length;
+
+        $rows.each(function(index, row) {
+          // Loop all cells
+          $(row).children("th").each(function(ind, cell) {
+            var $cell = $(cell);
+            if (($cell.attr("colspan") == 1 || typeof $cell.attr("colspan") == "undefined") &&
+              ($cell.attr("rowspan") == rowAmount - index ||
+                (index + 1 === rowAmount && typeof $cell.attr("rowspan") == "undefined"))) {
+              view.headerElements.push(cell);
+            }                
+          });                 
+        });
+
+        // Sort array
+        view.headerElements.sort(function(lhs, rhs){
+          return parseInt($(lhs).offset().left,10) - parseInt($(rhs).offset().left,10);
+        });
+      }
+
+      // Verification
+      var unrenderableColCnt = view.columns.where({ renderable: false}).length;
+      if (view.headerElements.length != (view.columns.size() - unrenderableColCnt)) {
+        throw new RangeError("cannot determine header elements for every column.");
+      }
+    }
+  });
+
+}));
