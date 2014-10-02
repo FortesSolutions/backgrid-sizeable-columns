2ce3e1636f6bbf60af7122cb0eb5699ab699bbba
Wilbert van de Ridder <wilbert@numbat.(none)>
2014-10-02 15:16:23 +0200

    Size columns plugin update; lots of fixes;

--- backgrid-sizeable-columns.js
+++ backgrid-sizeable-columns.js
@@ -12,7 +12,9 @@
     module.exports = factory(require("underscore"), require("backgrid"));
   }
   // Browser
-  else factory(root._, root.Backgrid, root.moment);
+  else {
+    factory(root._, root.Backgrid, root.moment);
+  }
 
 }(this, function (_, Backgrid) {
   "use strict";
@@ -24,16 +26,16 @@
 
     /**
      Initializer.
-
      @param {Object} options
-     @param {Backbone.Collection.<Backgrid.Column>|Array.<Backgrid.Column>|Array.<Object>} options.columns Column metadata.
+     @param {Backgrid.Grid} options.grid Backgrid grid instance
      */
     initialize: function (options) {
-      this.columns =  options.columns;
-      this.collection = options.collection;
+      this.grid = options.grid;
 
       // Attach event listeners once on render
-      this.listenTo(this.collection, "backgrid:header:rendered", this.render);
+      this.listenTo(this.grid.header, "backgrid:header:rendered", this.render);
+      this.listenTo(this.grid.columns, "width:auto", this.setWidthAuto);
+      this.listenTo(this.grid.columns, "width:fixed", this.setWidthFixed);
     },
 
     /**
@@ -43,14 +45,14 @@
       var view = this;
       view.$el.empty();
 
-      this.columns.each(function(col) {
+      view.grid.columns.each(function (col) {
         if (typeof col.get("renderable") == "undefined" || col.get("renderable")) {
           var $colEl = $("<col>").appendTo(view.$el).attr("data-column-id", col.cid);
           var colWidth = col.get("width");
           var colMinWidth = col.get("minWidth");
           var colMaxWidth = col.get("maxWidth");
 
-          if (colWidth) {
+          if (colWidth && colWidth != "*") {
             if (colMinWidth && colWidth < colMinWidth) {
               colWidth = colMinWidth;
             }
@@ -63,9 +65,34 @@
       });
 
       // Trigger event
-      view.collection.trigger("backgrid:colgroup:updated");
-
+      view.grid.collection.trigger("backgrid:colgroup:updated");
       return this;
+    },
+    getColumnElement: function (colModel) {
+      return this.$el.find('col[data-column-id="' + colModel.cid + '"]');
+    },
+    setWidthAuto: function (colModel) {
+      // Get column element
+      var $colElement = this.getColumnElement(colModel);
+
+      // Save width
+      colModel.set("width", "*");
+
+      // Set column width to auto
+      $colElement.css("width", "");
+    },
+    setWidthFixed: function (colModel) {
+      // Get column element
+      var $colElement = this.getColumnElement(colModel);
+
+      // Get width of header element
+      var width = this.grid.header.$el.find("th[data-columnname='" + colModel.get("name") + "']").outerWidth();
+
+      // Set column width to the original width
+      $colElement.css("width", width);
+
+      // Save width
+      colModel.set("width", width);
     }
   });
 
@@ -78,11 +105,12 @@
      @param {Backbone.Collection.<Backgrid.Column>|Array.<Backgrid.Column>|Array.<Object>} options.columns Column metadata.
      */
     initialize: function (options) {
-      this.grid = options.grid;
-      this.columns =  this.grid.columns;
-      this.header = this.grid.header;
       this.sizeAbleColumns = options.sizeAbleColumns;
-      this.saveModelWidth = options.saveModelWidth;
+      this.grid = this.sizeAbleColumns.grid;
+      this.columns = this.grid.columns;
+      this.header = this.grid.header;
+
+      this.saveColumnWidth = options.saveColumnWidth;
       this.setHeaderElements();
       this.attachEvents();
     },
@@ -94,22 +122,14 @@
       var view = this;
       view.$el.empty();
 
-      // Event helper function
-      var stopEvent = function(e) {
-        if(e.stopPropagation) e.stopPropagation();
-        if(e.preventDefault) e.preventDefault();
-        e.cancelBubble=true;
-        e.returnValue=false;
-      };
-
       // For now, loop tds in first row
-      _.each(view.headerElements, function(columnEl, index) {
+      _.each(view.headerElements, function (columnEl, index) {
         // Get matching col element
         var $column = $(columnEl);
         var $col = view.sizeAbleColumns.$el.find("col").eq(index);
         var columnModel = view.columns.get($col.data("column-id"));
 
-        if (columnModel.get("resizeAble") &&
+        if (columnModel.get("resizeable") &&
           (typeof columnModel.get("renderable") == "undefined" || columnModel.get("renderable"))) {
 
           // Create helper elements
@@ -123,8 +143,8 @@
             .appendTo($resizeHandler);
 
           // Make draggable
-          $resizeHandler.on("mousedown", function(e) {
-            stopEvent(e);
+          $resizeHandler.on("mousedown", function (e) {
+            view._stopEvent(e);
             var startX = $resizeHandler.offset().left;
             var $doc = $(document);
             var handlerNonDragSize = $resizeHandler.outerWidth();
@@ -134,8 +154,8 @@
             $resizeHandlerHelper.show();
 
             // Follow the mouse
-            var mouseMoveHandler = function(evt) {
-              stopEvent(evt);
+            var mouseMoveHandler = function (evt) {
+              view._stopEvent(evt);
 
               // Check for constraints
               var minWidth = columnModel.get("minWidth");
@@ -162,9 +182,9 @@
             $doc.on("mousemove", mouseMoveHandler);
 
             // Add handler to listen for mouseup
-            var mouseUpHandler = function(evt) {
+            var mouseUpHandler = function (evt) {
               // Cleanup
-              stopEvent(evt);
+              view._stopEvent(evt);
               $resizeHandler.removeClass("grid-draggable");
               $resizeHandlerHelper.hide();
               $doc.off("mouseup", mouseUpHandler);
@@ -175,22 +195,19 @@
               var offset = (startX - stopX);
               var oldWidth = $column.outerWidth();
               var newWidth = oldWidth - offset;
-
-              // Set column width
               $col.width(newWidth);
-              newWidth = $column.outerWidth();
-              $col.width(newWidth);
-              view.updateHandlerPosition();
 
+              // Save width and trigger events
               if (newWidth != oldWidth) {
-                if (view.saveModelWidth) {
+                if (view.saveColumnWidth) {
                   // Save updated width
-                  columnModel.set("width", newWidth, { silent: true});
+                  columnModel.set("width", newWidth, {silent: true});
                 }
 
                 // Trigger event
                 view.columns.trigger("resize", columnModel, newWidth, oldWidth);
               }
+              view.updateHandlerPosition();
             };
             $doc.on("mouseup", mouseUpHandler);
           });
@@ -202,23 +219,77 @@
 
       return this;
     },
-    attachEvents: function() {
+    _stopEvent: function (e) {
+      if (e.stopPropagation) {
+        e.stopPropagation();
+      }
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+      e.cancelBubble = true;
+      e.returnValue = false;
+    },
+    attachEvents: function () {
       var view = this;
-      view.listenTo(view.columns, "change:resizeAble", view.render);
-      view.listenTo(view.columns, "change:width", view.updateHandlerPosition);
+      view.listenTo(view.columns, "change:resizeable", view.render);
+      view.listenTo(view.columns, "resize width:auto width:fixed add remove", view.checkSpacerColumn);
+      view.listenTo(view.columns, "width:auto width:fixed", view.updateHandlerPosition);
       view.listenTo(view.grid.collection, "backgrid:refresh", view.render);
-      view.listenTo(view.grid.collection, "backgrid:colgroup:updated", function() {
+      view.listenTo(view.grid.collection, "backgrid:colgroup:updated", function () {
         // Wait for callstack to be cleared
-        // TODO: see if we can do without this delay function
-        _.delay(function() {
+        _.defer(function () {
           view.setHeaderElements();
           view.render();
-        }, 0);
+        });
       });
     },
-    updateHandlerPosition: function() {
+    checkSpacerColumn: function () {
       var view = this;
-      _.each(view.headerElements, function(columnEl, index) {
+      var spacerColumn = _.first(view.columns.where({name: "__spacerColumn"}));
+      var autoColumns = view.columns.filter(function (col) {
+        return col.get("width") == "*" && col.get("name") != "__spacerColumn";
+      });
+
+      // Check if there is a column with auto width, if so, no need to do anything
+      if (_.isEmpty(autoColumns)) {
+        var totalWidth = view.columns.reduce(function (memo, num) {
+          var colWidth = (num.get("width") == "*") ? 0 : num.get("width");
+          return memo + colWidth;
+        }, 0);
+        var gridWidth = view.grid.$el.width();
+
+        if (gridWidth > totalWidth) {
+          // The grid is larger than the cumulative column width, we need a spacer column
+          if (!spacerColumn) {
+            // Create new column model
+            view.columns.add({
+              name: "__spacerColumn",
+              label: "",
+              editable: false,
+              cell: Backgrid.StringCell,
+              width: "*",
+              nesting: [],
+              resizeable: false,
+              sortable: false,
+              orderable: false,
+              position: 9999
+            });
+          }
+        }
+        else {
+          // Cumulative column width exceeds grid width, no need for a spacerColumn.
+          if (spacerColumn) {
+            view.columns.remove(spacerColumn);
+          }
+        }
+      }
+      else if (spacerColumn) {
+        view.columns.remove(spacerColumn);
+      }
+    },
+    updateHandlerPosition: function () {
+      var view = this;
+      _.each(view.headerElements, function (columnEl, index) {
         var $column = $(columnEl);
 
         // Get handler for current column and update position
@@ -226,7 +297,7 @@
           .css("left", $column.position().left + $column.outerWidth());
       });
     },
-    setHeaderElements: function() {
+    setHeaderElements: function () {
       var view = this;
       var $headerEl = view.grid.header.$el;
       var $rows = $headerEl.children("tr");
@@ -239,30 +310,23 @@
       else {
         // Get all rows in the header
         var rowAmount = $rows.length;
-        $rows.each(function(index, row) {
+        $rows.each(function (index, row) {
           // Loop all cells
-          $(row).children("th").each(function(ind, cell) {
+          $(row).children("th").each(function (ind, cell) {
             var $cell = $(cell);
             if (($cell.attr("colspan") == 1 || typeof $cell.attr("colspan") == "undefined") &&
               ($cell.attr("rowspan") == rowAmount - index ||
                 (index + 1 === rowAmount && typeof $cell.attr("rowspan") == "undefined"))) {
               view.headerElements.push(cell);
             }
-          });                 
+          });
         });
 
         // Sort array
-        view.headerElements.sort(function(lhs, rhs){
-          return parseInt($(lhs).offset().left,10) - parseInt($(rhs).offset().left,10);
+        view.headerElements.sort(function (lhs, rhs) {
+          return parseInt($(lhs).offset().left, 10) - parseInt($(rhs).offset().left, 10);
         });
-      }
-
-      // Verification
-      var unrenderableColCnt = view.columns.where({ renderable: false}).length;
-      if (view.headerElements.length != (view.columns.size() - unrenderableColCnt)) {
-        throw new RangeError("cannot determine header elements for every column.");
       }
     }
   });
-
 }));
